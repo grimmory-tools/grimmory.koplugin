c4fa59a0a149b3862e8eb165678a3d540e73932c
James Ward <james@notjam.es>
2026-07-07 20:07:16 -0400

    fix: resolve _meta from local plugin path

--- spec/grimmory/plugin_metadata_spec.lua
+++ spec/grimmory/plugin_metadata_spec.lua
@@ -1,33 +1,36 @@
 package.path = "grimmory.koplugin/?.lua;" .. package.path
 
-local fake_package_meta = {
-
-}
-
-package.preload["_meta"] = function()
-    return fake_package_meta
+package.preload["gettext"] = function()
+    return function(text)
+        return text
+    end
 end
 
-package.preload["cache"] = function()
+package.preload["datastorage"] = function()
     return {
-        new = function()
-            local cache = {}
 
-            return {
-                get = function(key)
-                    return cache[key]
-                end,
-                insert = function(key, value)
-                    cache[key] = value
-                end
-            }
-        end,
     }
 end
+
+local fake_package_meta = {}
 
 local GrimmoryPluginMetadata = require("grimmory/plugin_metadata")
 
 describe("GrimmoryPluginMetadata", function()
+    local original_get_meta = nil
+
+    before_each(function()
+        original_get_meta = GrimmoryPluginMetadata.getMeta
+
+        GrimmoryPluginMetadata.getMeta = function()
+            return fake_package_meta
+        end
+    end)
+
+    after_each(function()
+        GrimmoryPluginMetadata.getMeta = original_get_meta
+    end)
+
     describe("hasRepository", function()
         it("returns false when repository field is missing", function()
             assert.are.equal(GrimmoryPluginMetadata:hasRepository(), false)
